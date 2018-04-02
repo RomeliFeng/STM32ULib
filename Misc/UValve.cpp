053f7209118ea9fe8d6a31b9e60c0766f46a5cff
romeli <fengzheng1995@gmail.com>
2018-04-02 17:21:00 +0800

    添加了电磁阀控制模组

---
+++ Misc/UValve.cpp
@@ -0,0 +1,77 @@
+/*
+ * UValve.cpp
+ *
+ *  Created on: 2018年4月2日
+ *      Author: Romeli
+ */
+
+#include <Misc/UValve.h>
+
+UValve::UValve(uint8_t valveNum, bool inverting) {
+	_dataSize = valveNum / 8 + 1;
+	_data = new BytetoBit_Typedef[_dataSize]();
+	SetInverting(inverting);
+}
+
+UValve::~UValve() {
+	delete[] _data;
+}
+
+void UValve::Open() {
+	uint8_t status = _inverting ? 0x00 : 0xff;
+	for (uint8_t i = 0; i < _dataSize; ++i) {
+		_data[i].byte = status;
+	}
+}
+
+void UValve::Open(uint8_t valveNo) {
+	//求第几个字节
+	uint8_t index = valveNo >> 3;
+	//计算掩码
+	uint8_t mask = uint8_t(1) << (valveNo % 8);
+	if (_inverting) {
+		_data[index].byte &= (~mask);
+	} else {
+		_data[index].byte |= mask;
+	}
+}
+
+void UValve::Close() {
+	uint8_t status = _inverting ? 0xff : 0x00;
+	for (uint8_t i = 0; i < _dataSize; ++i) {
+		_data[i].byte = status;
+	}
+}
+
+void UValve::Close(uint8_t valveNo) {
+	//求第几个字节
+	uint8_t index = valveNo >> 3;
+	//计算掩码
+	uint8_t mask = uint8_t(1) << (valveNo % 8);
+	if (_inverting) {
+		_data[index].byte |= mask;
+	} else {
+		_data[index].byte &= (~mask);
+	}
+}
+
+void UValve::Control(uint8_t* status) {
+	for (uint8_t i = 0; i < _dataSize; ++i) {
+		_data[i].byte = _inverting ? ~status[i] : status[i];
+	}
+}
+
+void UValve::Control(uint8_t* status, bool onOff) {
+	for (uint8_t i = 0; i < _dataSize; ++i) {
+		if ((_inverting && onOff) || ((!_inverting) && (!onOff))) {
+			//当反向并且打开  或者 未反向并且关闭
+			_data[i].byte &= (~status[i]);
+		} else {
+			_data[i].byte |= status[i];
+		}
+	}
+}
+
+void UValve::SetInverting(bool inverting) {
+	_inverting = inverting;
+}
---
+++ Misc/UValve.h
@@ -0,0 +1,36 @@
+/*
+ * UValve.h
+ *
+ *  Created on: 2018年4月2日
+ *      Author: Romeli
+ */
+
+#ifndef MISC_UVALVE_H_
+#define MISC_UVALVE_H_
+
+#include <Misc/UMisc.h>
+
+class UValve {
+public:
+	UValve(uint8_t valveNum, bool inverting = false);
+	virtual ~UValve();
+
+	virtual void Init() = 0;
+
+	void Open();
+	void Open(uint8_t valveNo);
+	void Close();
+	void Close(uint8_t valveNo);
+	void Control(uint8_t* status);
+	void Control(uint8_t* status, bool onOff);
+
+	void SetInverting(bool inverting);
+protected:
+	volatile BytetoBit_Typedef* _data;
+	uint8_t _dataSize;
+	bool _inverting;
+
+	virtual void Apply() = 0;
+};
+
+#endif /* MISC_UVALVE_H_ */
