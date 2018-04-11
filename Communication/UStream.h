98529214dce4ea92f8772c7a2ead478a1dde089a
Romeli <Romeli@Romeli-LP.localdomain>
2018-04-11 23:11:29 +0800

    使用模板实现多态接口

--- Communication/UStream.h
+++ Communication/UStream.h
@@ -89,145 +89,14 @@ public:
 	Status_Typedef PeekNextDigital(uint8_t* data, uint8_t ignore,
 	        bool detectDecimal = false);
 
-	/*
-	 * author Romeli
-	 * explain 从流中读取一个整形数
-	 * param num 读取的整数存放位置
-	 * param ignore 忽略的字符
-	 * return Status_Typedef 是否成功
-	 */
+	//使用模板实现多态接口
 	template<typename T>
 	Status_Typedef NextInt(T& num, uint8_t ignore = 0) {
-		bool firstChar = true;
-		bool isNeg = false;
-		uint8_t c = 0;
-		uint16_t sp = _rxBuf.start;
-		int32_t n = 0;
-
-		while (Available() > 0) {
-			if (PeekNextDigital(&c, ignore, false) == Status_Ok) {
-				//如果读到数字
-				if (c == '-') {
-					if (firstChar) {
-						//检测到一个'-'
-						isNeg = true;
-						SpInc(_rxBuf);
-						continue;
-					} else {
-						//'-'不是第一个数
-						break;
-					}
-				} else if (c == '+') {
-					if (firstChar) {
-						//检测到一个'+'
-						SpInc(_rxBuf);
-						continue;
-					} else {
-						//'+'不是第一个数
-						break;
-					}
-				} else if ((c == ignore) && (ignore != 0)) {
-					SpInc(_rxBuf);
-					continue;
-				}
-				n = n * 10 + c - '0';
-				firstChar = false;
-				SpInc(_rxBuf);
-			} else {
-				break;
-			}
-		}
-		if ((sp != _rxBuf.start) && (c != '-') && (c != ignore)) {
-			//有读取到数
-			if (isNeg) {
-				n = -n;
-			}
-			num = n;
-			return Status_Ok;
-		} else {
-			//没有读取到数
-			num = 0;
-			return Status_Error;
-		}
+		return nextInt(num, ignore);
 	}
-
-	/*
-	 * author Romeli
-	 * explain 从流中读取一个浮点数
-	 * param flo 读取的浮点数存放位置
-	 * param ignore 忽略的字符
-	 * return Status_Typedef 是否成功
-	 */
 	template<typename T>
-	Status_Typedef NextDouble(T flo, uint8_t ignore = 0) {
-		double f = 0;
-		double frac = 1.0;
-		bool isNeg = false;
-		bool isFra = false;
-		bool firstChar = true;
-		uint16_t sp = _rxBuf.start;
-		uint8_t c = 0;
-
-		while (Available() > 0) {
-			if (PeekNextDigital(&c, ignore, true) == Status_Ok) {
-				if (c == '-') {
-					if (firstChar) {
-						//检测到一个'-'
-						isNeg = true;
-						SpInc(_rxBuf);
-						continue;
-					} else {
-						//'-'不是第一个数
-						break;
-					}
-				} else if (c == '+') {
-					if (firstChar) {
-						//检测到一个'+'
-						SpInc(_rxBuf);
-						continue;
-					} else {
-						//'-'不是第一个数
-						break;
-					}
-				} else if ((c == ignore) && (ignore != 0)) {
-					SpInc(_rxBuf);
-					continue;
-				} else if (c == '.') {
-					if (isFra) { //不应出现两个'-'
-						break;
-					} else {
-						if (!firstChar) {
-							SpInc(_rxBuf);
-							isFra = true;
-							continue;
-						} else {
-							//第一个字符为点的时候
-							break;
-						}
-					}
-				}
-				if (isFra) {
-					frac *= 0.1;
-				}
-				f = f * 10 + c - '0';
-				SpInc(_rxBuf);
-				firstChar = false;
-			} else {
-				break;
-			}
-		}
-
-		if ((sp != _rxBuf.start) && (c != '-') && (c != ignore)) {
-			//有读取到数
-			f = isNeg ? -f : f;
-			f = isFra ? f * frac : f;
-			flo = f;
-			return Status_Ok;
-		} else {
-			//没有读取到数
-			flo = 0;
-			return Status_Error;
-		}
+	Status_Typedef NextFloat(T& flo, uint8_t ignore = 0) {
+		return nextFloat(flo, ignore);
 	}
 
 	virtual uint16_t Available();
@@ -259,6 +128,8 @@ protected:
 	void DMASend(uint8_t *&data, uint16_t &len);
 	void DMAReceive(uint8_t *&data, uint16_t &len);
 private:
+	Status_Typedef nextInt(int64_t& num, uint8_t ignore = 0);
+	Status_Typedef nextFloat(double& flo, uint8_t ignore = 0);
 };
 
 #endif /* USTEAM_H_ */
