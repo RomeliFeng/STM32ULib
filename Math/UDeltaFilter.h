1d691023d386ec42fb98c32963690eea804fbea5
tromeli <fengzheng1995@gmail.com>
2018-06-23 16:05:26 +0800

    修复内循环判断的错误

--- Math/UDeltaFilter.h
+++ Math/UDeltaFilter.h
@@ -47,40 +47,38 @@ public:
 	 * return T
 	 */
 	T Get() {
-		T delta = *_Input - _Last; //计算一阶导数
-		T deltaError = delta - _LastDelta; //计算二阶导数
+		T delta = *_Input - _Last; //计算外循环一阶导数
+		T delta2 = *_Input - _Last2; //计算内循环一阶导数
+		T deltaError = delta - _LastDelta; //计算外循环二阶导数
+		T deltaError2 = delta2 - _LastDelta2; //计算内循环二阶导数
 		deltaError = deltaError < 0 ? -deltaError : deltaError; //正值化
+		deltaError2 = deltaError2 < 0 ? -deltaError2 : deltaError2; //正值化
 
 		if (deltaError > _MaxDeltaError) {
 			//保持历史最大值
 			_MaxDeltaError = deltaError;
 		}
+		//内循环一直保留上次的值
+		_Last2 = *_Input; //保留上次的值
+		_LastDelta2 = delta; //保留上次的一阶导数
+
 		if (deltaError < _DeltaErrorLimit) {
-			//变化趋势小于限定值,直接返回记录当前趋势（二阶导数）
+			//外循环变化趋势小于限定值,直接返回记录当前趋势（二阶导数）
 			_Last = *_Input;
 			_LastDelta = delta;
-		} else {
-			//变化趋势超过限定值，当前数值可能无效，进入内循环判定
-			//如果当前数值无效，下一数值应脱离内循环
-			delta = *_Input - _Last2; //计算内循环一阶导数
-			deltaError = delta - _LastDelta2; //计算内循环二阶导数
-			deltaError = deltaError < 0 ? -deltaError : deltaError; //正值化
-
-			_Last2 = *_Input; //保留上次的值
-			_LastDelta2 = delta; //保留上次的一阶导数
-			if (deltaError < _DeltaErrorLimit) {
-				//如果内循环变化率小于限定值，累计次数进行逃脱
-				++_EscapeCount;
-				if (_EscapeCount >= _EscapeTimes) {
-					//到达指定逃脱次数，逃脱内循环
-					_Last = *_Input;
-					_LastDelta = delta;
-				}
-			} else {
-				//变化率依旧大于限定值，清空累计
-				_EscapeCount = 0;
+		} else if (deltaError < _DeltaErrorLimit) {
+			//如果内循环变化率小于限定值，累计次数进行逃脱
+			++_EscapeCount;
+			if (_EscapeCount >= _EscapeTimes) {
+				//到达指定逃脱次数，逃脱内循环
+				_Last = *_Input;
+				_LastDelta = delta;
 			}
+		} else {
+			//变化率依旧大于限定值，清空累计
+			_EscapeCount = 0;
 		}
+
 		return _Last;
 	}
 private:
